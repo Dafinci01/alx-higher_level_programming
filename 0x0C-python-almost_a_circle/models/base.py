abb5717a010b2b539406f1d3367ad1197d497a84
Dafinci <odelanadavid@gmail.com>
2024-04-23 08:11:27 -0700

    updated

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -11,7 +11,10 @@ class Base:
     Represents the base model
     """
     __nb_objects = 0
+
     def __init__(self, id=None):
+        """class constructor
+        """
         if id is not None:
             self.id = id
         else:
