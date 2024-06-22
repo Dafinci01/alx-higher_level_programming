8e852c05f8d1c79f2ff81da0bb46133eb97ea65f
Dafinci01 <odelanadavid@gmail.com>
2024-06-22 16:11:01 +0100

    multi languages

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,11 @@
-#!/usr/bin/node 
+#!/usr/bin/node
 
 const args = process.argv.slice(2);
 
-
-if (process.argv.length === 1){
-	console.log('No argument');
-}else if (process.argv.length === 1){
-	console.log('Argument foun');
-}else {
-	console.log('Arguments found');
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
