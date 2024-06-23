d186e83e33cec31cbf861dc62eee1b7a3c8267c1
Dafinci01 <odelanadavid@gmail.com>
2024-06-23 23:23:33 +0100

    javascripy update

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,8 +2,8 @@
 
 const args = process.argv.slice(2);
 
-if(process.arg[0] === undefined){
-	console.log("No argument");
-}else {
-	console.log(process.argv[0]);
+if (args[0] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(args[0]);
 }
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,15 +1,11 @@
-#!/usr/bin/node 
-
-
+#!/usr/bin/node
 
 const args = process.argv.slice(2);
 
 const number = Number(args[0]);
 
-
-if (Number is NaN(number)) {
+if (Number.isNaN(number)) {
   console.log('Not a number');
 } else {
   console.log(`My number: ${number}`);
 }
-
