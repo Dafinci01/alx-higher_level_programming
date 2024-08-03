c8d209be8ab2f64a4e857ba3fdc5edd46000c9cd
Dafinci <odelanadavid@gmail.com>
2024-08-02 22:36:16 -0700

    script to request http response

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Send a DELETE request to a given URL and display the response body.
-curl -sX DELETE "$1"
+methods=$(curl -s -X OPTIONS "\$1" -I | grep -i "Allow:" | cut -d' ' -f2-)
+
---
+++ 0x10-python-network_0/README.md
@@ -0,0 +1,1 @@
+this is python network task using bash to check http protocols
